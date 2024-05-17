89bc3a30f419a0c43b0793f598ad25cdbad5820d
Phùng Quang Anh <tenzovn@gmail.com>
2024-05-17 17:24:38 +0700

    add page icon

---
+++ .history/main_20240517170410.js
@@ -0,0 +1,23 @@
+const admin = {
+    username: 'tenzovn',
+    password: 'anhdaik110'
+}
+
+const links = {
+    fb: 'https://www.facebook.com/enqungphug',
+    ig: 'https://Instagram.com/enqungphug' ,
+    github: 'https://github.com/tenzovn' ,
+    linkedin: 'https://www.linkedin.com/in/enqungphug/',
+}
+const fbLink = document.getElementById('fbLink');
+const igLink = document.getElementById('igLink');
+const githubLink = document.getElementById('githubLink');
+const githubLink2 = document.getElementById('githubLink2');
+const linkedinLink = document.getElementById('linkedinLink');
+
+fbLink.setAttribute('href', links.fb);
+igLink.setAttribute('href', links.ig);
+githubLink.setAttribute('href', links.github);
+githubLink2.setAttribute('href', links.github);
+linkedinLink.setAttribute('href', links.linkedin);
+
